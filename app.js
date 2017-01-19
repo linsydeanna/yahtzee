27ceaba6445be881bfcc1aaef2f571bed693c482
Linsy <Linsy.Joyner@gmail.com>
2017-01-19 10:32:10 -0500

    dice are rolling without duplication

--- app.js
+++ app.js
@@ -1,30 +1,12 @@
 $(document).ready(function() {
 
-function rollDice() {
-  for (var i = 1; i < 6; i++) {
-
-    console.log("i is ", i);
-
-    var $dieNumber = Math.floor( Math.random() * 6 ) + 1;
-    console.log($dieNumber);
-
-    var $die = $( "<div class=' dice-image dice-" + i + "'></div>" )
-    console.log("die is ", $die)
-    $(".dice").append($die)
-    console.log("die is ", $die)
-
-    var $image = document.createElement('img');
-    console.log($image);
-    $image.src = "images/" + $dieNumber + ".svg";
-    $(".dice-" + i + "").append($image);
-
-  }
-
-}
-
-rollDice();
-
-
-
+  $( "#roll-button" ).click(function() {
+    for (var i = 1; i < 6; i++) {
+      var $dieNumber = Math.floor( Math.random() * 6 ) + 1;
+      var $die = document.getElementById("die-" + i + "");
+      $die.src = "images/" + $dieNumber + ".svg";
+      $(".dice-" + i + "").append($die);
+    }
+  });
 
 })
