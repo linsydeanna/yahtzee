3b2b3f4869e6b82f31b23b1fc0930d91adf7ac0d
Linsy <Linsy.Joyner@gmail.com>
2017-01-19 09:52:57 -0500

    dice are rolling

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-var dieNumber;
+$(document).ready(function() {
 
 function rollDice() {
-  dieNumber = Math.floor( Math.random() * 6 ) + 1
-  document.querySelector('.dice').innerHTML = '<img src="images/' + dieNumber + '.svg" class="dice-image" alt="">'
-  console.log(dieNumber)
+  for (var i = 1; i < 6; i++) {
+
+    console.log("i is ", i);
+
+    var $dieNumber = Math.floor( Math.random() * 6 ) + 1;
+    console.log($dieNumber);
+
+    var $die = $( "<div class=' dice-image dice-" + i + "'></div>" )
+    console.log("die is ", $die)
+    $(".dice").append($die)
+    console.log("die is ", $die)
+
+    var $image = document.createElement('img');
+    console.log($image);
+    $image.src = "images/" + $dieNumber + ".svg";
+    $(".dice-" + i + "").append($image);
+
+  }
+
 }
 
-rollDice()
+rollDice();
 
-for (var i = 1; i < 6; i++) {
-  console.log(i)
-}
+
+
+
+})
