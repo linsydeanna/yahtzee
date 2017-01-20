aeb6ee5e3c9e65f3e66debd28c9cb1a1dfd9c919
Linsy <Linsy.Joyner@gmail.com>
2017-01-20 17:34:18 -0500

    grabbing value of die and adding class on click

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ $(document).ready(function() {
     for (var i = 1; i < 6; i++) {
       var $dieNumber = Math.floor( Math.random() * 6 ) + 1;
       var $die = document.getElementById("die-" + i + "");
+      $die.setAttribute("dievalue", $dieNumber)
       $die.src = "images/" + $dieNumber + ".svg";
-      $(".dice-" + i + "").append($die);
+      $(".die-" + i + "").append($die);
     }
+  });
+
+  $( ".die" ).click(function() {
+    $(event.target).addClass( "die-kept" );
+    var $attribute = event.target.getAttribute("dievalue");
+    console.log("attribute of die is ", $attribute)
   });
 
 })
