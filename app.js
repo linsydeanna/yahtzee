78a447664782ef3915dd61fb471e378fab1fdad1
Linsy <Linsy.Joyner@gmail.com>
2017-01-22 09:58:43 -0500

    selected dice remain in between rolls and number of rolls are being incremented

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 $(document).ready(function() {
 
+  var $diceValue = [];
+  var $rolls = 0;
+
   $( "#roll-button" ).click(function() {
-    for (var i = 1; i < 6; i++) {
-      var $dieNumber = Math.floor( Math.random() * 6 ) + 1;
-      var $die = document.getElementById("die-" + i + "");
-      $die.setAttribute("dievalue", $dieNumber)
-      $die.src = "images/" + $dieNumber + ".svg";
-      $(".die-" + i + "").append($die);
-    }
-  });
+
+      for (var i = 1; i < 6; i++) {
+        var $die = document.getElementById("die-" + i + "");
+        if ($($die).hasClass( "die-kept" )) {
+          var $attribute = $die.getAttribute("dievalue");
+          $diceValue.push({
+            die: $die.id,
+            value: $attribute
+          });
+        } else {
+          var $dieNumber = Math.floor( Math.random() * 6 ) + 1;
+          $die.setAttribute("dievalue", $dieNumber);
+          $die.src = "images/" + $dieNumber + ".svg";
+          $(".die-" + i + "").append($die);
+        };
+      };
+      $rolls += 1;
+    });
 
   $( ".die" ).click(function() {
-    $(event.target).addClass( "die-kept" );
-    var $attribute = event.target.getAttribute("dievalue");
-    console.log("attribute of die is ", $attribute)
+    if ($(event.target).hasClass( "die-kept" )) {
+      $(event.target).removeClass( "die-kept" );
+    } else {
+      $(event.target).addClass( "die-kept" );
+    };
   });
 
-})
+});
