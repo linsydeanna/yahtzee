9999e556f019d7fa72d8d62770cf75792e737d42
Linsy <Linsy.Joyner@gmail.com>
2017-01-22 20:18:49 -0500

    created functions for each scoring category

--- app.js
+++ app.js
@@ -15,10 +15,6 @@ $(document).ready(function() {
           $die.src = "images/" + $dieNumber + ".svg";
           $(".die-" + i + "").append($die);
         };
-        if ($rolls === 2) {
-          var $attribute = $die.getAttribute("dievalue");
-          $diceValue.push(parseInt($attribute));
-        };
       };
     };
     $rolls += 1;
@@ -38,21 +34,53 @@ $(document).ready(function() {
     $('img').removeClass( "die-kept" );
   });
 
-  $( ".upper" ).click(function() {
-    var $upper = document.getElementsByClassName('upper');
+  $( ".total-number" ).click(function() {
+    var $totalNumber = document.getElementsByClassName('total-number');
+    console.log($totalNumber)
+    for (var i = 1; i < 6; i++) {
+      var $die = document.getElementById("die-" + i + "");
+      var $attribute = $die.getAttribute("dievalue");
+      $diceValue.push(parseInt($attribute));
+    }
     for (i = 0; i < 6; i++) {
-      if (event.target === $upper[i]) {
+      if (event.target === $totalNumber[i]) {
         function matchNumber(die) {
           return die === (i + 1);
         };
         var $score = $diceValue.filter(matchNumber);
+        console.log($score)
       };
+      console.log($score)
     };
     function add(a, b) {
       return a + b;
     };
+    console.log($score)
     var $totalScore = $score.reduce(add, 0);
     event.target.innerHTML = $totalScore;
   });
+
+  $( ".specified" ).click(function() {
+    var $itemScore = event.target.getAttribute("data-score");
+    console.log($itemScore);
+    event.target.innerHTML = $itemScore;
+  });
+
+  $( ".total" ).click(function() {
+    console.log("diceValue is ", $diceValue);
+    for (var i = 1; i < 6; i++) {
+      var $die = document.getElementById("die-" + i + "");
+      var $attribute = $die.getAttribute("dievalue");
+      $diceValue.push(parseInt($attribute));
+      console.log("$diceValue in loop is ", $diceValue)
+    }
+    console.log("diceValue after is ", $diceValue);
+    function add(a, b) {
+      return a + b;
+    };
+    var $totalScore = $diceValue.reduce(add, 0);
+    event.target.innerHTML = $totalScore;
+  });
+
 
 });
